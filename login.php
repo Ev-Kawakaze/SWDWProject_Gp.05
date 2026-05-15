721eb9bda33ae8784576cca970ecb327829eb092
imxiancon <2559807690@qq.com>
2026-05-15 17:24:12 +0800

    上传我的代码

---
+++ login.php
@@ -0,0 +1,46 @@
+<?php session_start(); ?>
+<?php
+include 'connectDB.php';
+/*$servername = "localhost";
+$username 	= "root";
+$password 	= "";
+$db = "members";
+$conn = new mysqli($servername, $username, $password, $db);*/
+$msg = "";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { //https://www.w3ccoo.com/php/php_superglobals_request.html
+    $user = $_POST["usr"];
+    $pwd  = $_POST["pwd"];
+    $sql = "SELECT * FROM members WHERE username = '$user' AND password = '$pwd'";
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result)>0){
+        $_SESSION['user'] = $user;
+        $msg = "$user and $pwd are found in the DB. Login successful!";
+    }else{
+        $msg = "$user and $pwd are NOT found in the DB. Login failed!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+    <h2>Logon Form</h2>
+
+    <?php if ($msg != "") { ?>
+        <p>
+            <?php echo $msg; ?>
+        </p>
+    <?php } ?>
+
+
+    <form action="" method="POST">
+        <label for="usr">username:</label><br>
+        <input type="text" id="usr" name="usr" placeholder="Username"><br>
+        <label for="pwd">password:</label><br>
+        <input type="password" id="pwd" name="pwd" placeholder="Password"><br><br>
+        <input type="submit" value="Submit">
+    </form> 
+</body>
+</html>
