b642180b6a92101b130a39f73ad27606c2b7ca49
User39771 <Jackie21916201@outlook.com>
2026-05-28 20:25:43 +0800

    Clean up session_check.php by removing comments
    Removed commented-out code regarding redirection.

--- session-logout/session_check.php
+++ session-logout/session_check.php
@@ -3,9 +3,6 @@ session_start();
 if(!(isset($_SESSION["username"]))){
     echo "Please login first";
     //https://www.w3schools.com/php/func_network_headers_sent.asp 
-    /*
-    要定向到的文件未知
-    */
     header("Location : login.php");
 }else{
     echo "Welcome, ".$_SESSION["username"];
