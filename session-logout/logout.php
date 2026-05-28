fd302ea3762d5320975f5dfd0c84b51489687e2a
User39771 <Jackie21916201@outlook.com>
2026-05-28 20:25:17 +0800

    Erase unnecessary comments

--- session-logout/logout.php
+++ session-logout/logout.php
@@ -3,8 +3,5 @@ include "01_ConnectDB.php";
 session_unset();
 session_destroy();
     //https://www.w3schools.com/php/func_network_headers_sent.asp 
-    /*
-    要定向到的文件未知
-    */
 header("Location : home.php");
 ?>
